cff26cee61a5099b0f3752b6f09054a9fdf176d0
Finallap <547805712@qq.com>
2019-10-16 21:42:52 +0800

    LeetCode Problem 1&2

---
+++ LeetCode/src/problem1.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class problem1 {
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement) && map.get(complement) != i)
+                return new int[]{map.get(complement), i};
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{2, 7, 11, 15};
+        int target = 9;
+        int[] results = twoSum(nums, target);
+        for (int result : results) {
+            System.out.print(result + " ");
+        }
+    }
+}
---
+++ LeetCode/src/problem2.java
@@ -0,0 +1,28 @@
+public class problem2 {
+//    public class ListNode{
+//        int val;
+//        ListNode next;
+//        ListNode(int x) { val = x; }
+//    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode current = dummyHead;
+        int carry = 0;
+        while (l1!=null||l2!=null||carry!=0){
+            int l1val = l1!=null?l1.val:0;
+            int l2val = l2!=null?l2.val:0;
+            int subVal = l1val+l2val+carry;
+            carry = subVal / 10;
+
+            ListNode sumNode = new ListNode(subVal%10);
+            current.next = sumNode;
+            current = sumNode;
+
+            if (l1!=null)l1=l1.next;
+            if (l2!=null)l2=l2.next;
+        }
+
+        return dummyHead.next;
+    }
+}
