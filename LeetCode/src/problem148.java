bcca48e23229dd0b4930c9d0f508ff2e46a133db
Finallap <547805712@qq.com>
2020-02-02 10:02:32 +0800

    LeetCode problem2_1：链表两数相加再尝试 LeetCode problem148：链表排序
    Signed-off-by: Finallap <547805712@qq.com>

---
+++ LeetCode/src/problem148.java
@@ -0,0 +1,63 @@
+public class problem148 {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) return head;
+
+//        快慢指针
+        ListNode fast = head;
+        ListNode slow = head;
+        ListNode breakN = null;
+        while (fast != null && fast.next != null) {
+            breakN = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        breakN.next = null;
+
+        ListNode left = sortList(head);
+        ListNode right = sortList(slow);
+        return listMerge(left, right);
+    }
+
+    public ListNode listMerge(ListNode left, ListNode right) {
+        ListNode dummyHead = new ListNode(-1);
+        ListNode curr = dummyHead;
+        while (left != null && right != null) {
+            if (left.val < right.val) {
+                ListNode node = new ListNode(left.val);
+                curr.next = node;
+                curr = curr.next;
+                left = left.next;
+            } else {
+                ListNode node = new ListNode(right.val);
+                curr.next = node;
+                curr = curr.next;
+                right = right.next;
+            }
+        }
+
+        while (left != null) {
+            ListNode node = new ListNode(left.val);
+            curr.next = node;
+            curr = curr.next;
+            left = left.next;
+        }
+
+        while (right != null) {
+            ListNode node = new ListNode(right.val);
+            curr.next = node;
+            curr = curr.next;
+            right = right.next;
+        }
+
+        return dummyHead.next;
+    }
+}
---
+++ LeetCode/src/problem2_1.java
@@ -0,0 +1,34 @@
+public class problem2_1 {
+    //Definition for singly-linked list.
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode root = new ListNode(0);
+        ListNode currentNode = root;
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+            int l1_val = l1 != null ? l1.val : 0;
+            int l2_val = l2 != null ? l2.val : 0;
+            int result = l1_val + l2_val + carry;
+            carry = result / 10;
+
+            ListNode node = new ListNode(result % 10);
+            currentNode.next = node;
+            currentNode = node;
+
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+
+        return root.next;
+    }
+}
+
