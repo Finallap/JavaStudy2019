92c2a2eaae71643cf570a51638549225f13095c0
Finallap <547805712@qq.com>
2020-02-05 22:34:46 +0800

    并发：几种AQS实现的同步器
    Signed-off-by: Finallap <547805712@qq.com>

---
+++ Concurrent/src/CountDownLatchExample.java
@@ -0,0 +1,34 @@
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class CountDownLatchExample {
+    private static final int threadCount = 500;
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService threadPool = Executors.newFixedThreadPool(300);
+        final CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+        for (int i = 0; i < threadCount; i++) {
+            final int threadNum = i;
+            threadPool.execute(() -> {
+                try {
+                    test(threadNum);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    countDownLatch.countDown();
+                }
+            });
+        }
+        countDownLatch.await();
+        threadPool.shutdown();
+        System.out.println("finish");
+    }
+
+    public static void test(int threadNum) throws InterruptedException {
+        Thread.sleep(1000);
+        System.out.println("threadnum:" + threadNum);
+        Thread.sleep(1000);
+
+    }
+}
