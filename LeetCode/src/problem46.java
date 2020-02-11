75ecd608e4c5ee5c51a1fffa756b20483db82816
Finallap <547805712@qq.com>
2020-02-11 14:01:30 +0800

    LeetCode problem46:回溯法全排列，还需要进一步理解
    Signed-off-by: Finallap <547805712@qq.com>

---
+++ LeetCode/src/problem46.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class problem46 {
+    //回溯法
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> results = new LinkedList<>();
+
+        //将数组转化为list
+        ArrayList<Integer> nums_list = new ArrayList<>();
+        for (int num : nums)
+            nums_list.add(num);
+
+        int len = nums.length;
+        backtrack(len, nums_list, results, 0);
+
+        return results;
+    }
+
+    private void backtrack(int len, ArrayList<Integer> nums, List<List<Integer>> results, int first) {
+        if (first == len)
+            results.add(new ArrayList<Integer>(nums));
+
+        for (int i = first; i < len; i++) {
+            Collections.swap(nums, first, i);
+            backtrack(len, nums, results, first + 1);
+            Collections.swap(nums, first, i);
+        }
+    }
+}
