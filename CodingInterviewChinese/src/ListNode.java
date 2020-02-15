5d0bc02d830992c323f26924022eb60493816d99
Finallap <547805712@qq.com>
2020-02-15 14:32:55 +0800

    剑指 problem6：链表，从尾到头打印链表
    Signed-off-by: Finallap <547805712@qq.com>

---
+++ CodingInterviewChinese/src/ListNode.java
@@ -0,0 +1,9 @@
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
---
+++ CodingInterviewChinese/src/problem6.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.Stack;
+
+//problem6：链表，从尾到头打印链表
+public class problem6 {
+    //    使用栈方法
+    public static ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+        ArrayList<Integer> result = new ArrayList<>();
+        Stack<Integer> stack = new Stack<>();
+        while (listNode != null) {
+            stack.push(listNode.val);
+            listNode = listNode.next;
+        }
+
+        while (!stack.empty())
+            result.add(stack.pop());
+
+        return result;
+    }
+
+    //    使用递归方法
+    public static ArrayList<Integer> printListFromTailToHead1(ListNode listNode) {
+        ArrayList<Integer> result = new ArrayList<>();
+        if (listNode != null) {
+            result.addAll(printListFromTailToHead1(listNode.next));
+            result.add(listNode.val);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        ListNode ln1 = new ListNode(1);
+        ListNode ln2 = new ListNode(2);
+        ListNode ln3 = new ListNode(3);
+        ListNode ln4 = new ListNode(4);
+        ListNode ln5 = new ListNode(5);
+        ListNode ln6 = new ListNode(6);
+        ListNode ln7 = new ListNode(7);
+        ListNode ln8 = new ListNode(8);
+        ln1.next = ln2;
+        ln2.next = ln3;
+        ln3.next = ln4;
+        ln4.next = ln5;
+        ln5.next = ln6;
+        ln6.next = ln7;
+        ln7.next = ln8;
+        ln8.next = null;
+        //ListNode ln1 = new ListNode();
+        //ln1=null;
+        System.out.println(printListFromTailToHead1(ln1));
+//        printListReverseinglyByRecursion(ln1);
+
+    }
+}
