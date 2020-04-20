3df36b7ab1e61ec5f3f9b8a310aa05211b60572c
Finallap <547805712@qq.com>
2020-04-20 13:01:44 +0800

    LeetCode Problem1143: 最长公共子序列，DP来解
    Signed-off-by: Finallap <547805712@qq.com>

---
+++ LeetCode/src/Problem1143.java
@@ -0,0 +1,24 @@
+/**
+ * @description: LeetCode Problem1143: 最长公共子序列，DP来解
+ * @date: 2020/4/20 12:57
+ * @author: Finallap
+ * @version: 1.0
+ */
+public class Problem1143 {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int len1 = text1.length();
+        int len2 = text2.length();
+
+        int[][] dp = new int[len1 + 1][len2 + 1];
+        for (int i = 1; i <= len1; i++) {
+            for (int j = 1; j <= len2; j++) {
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[len1][len2];
+    }
+}
