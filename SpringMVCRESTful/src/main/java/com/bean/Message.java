3905ad60826572b54e0b548c87aad3f76ae6a632
Finallap <547805712@qq.com>
2020-05-04 16:10:25 +0800

    尝试引入Lombok
    Signed-off-by: Finallap <547805712@qq.com>

--- SpringMVCRESTful/src/main/java/com/bean/Message.java
+++ SpringMVCRESTful/src/main/java/com/bean/Message.java
@@ -1,28 +1,19 @@
 package com.bean;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 /**
  * @description: Message
  * @date: 2020/5/1 15:24
  * @author: Finallap
  * @version: 1.0
  */
+@Getter
+@Setter
+@ToString
 public class Message {
     String name;
     String text;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
 }
